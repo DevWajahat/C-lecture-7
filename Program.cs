f20c772a2eebcf2c81bed9d462a671c64dd96e1e
Wajahat Abbas <155806602+DevWajahat@users.noreply.github.com>
2024-10-21 20:32:33 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace c__lecture_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate.Multiply(4, 5);
+        }
+    }
+
+    class Calculate
+    {
+        public static void add(int a , int b)
+        {
+            Console.WriteLine(a+b);
+        }
+
+        public static void Multiply(int a, int b)
+        {
+            Console.WriteLine((a * b));
+        }
+    }
+    class solve
+    {
+        public static void sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+    }
+    public class Employee
+    {
+        public string _employeeName;
+        public int _empAge;
+        public string _deptName;
+    }
+}
